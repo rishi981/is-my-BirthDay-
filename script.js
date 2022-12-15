c8da25cc3a3a4bfc91601ed466db1150c3665fda
rishi <oyerishi3234@gmail.com>
2022-12-15 11:01:49 +0530

    is your birthday is lucky

--- script.js
+++ script.js
@@ -7,7 +7,35 @@ const luckyNumber = document.querySelector
 const checkNumberButton = document.querySelector
 ("#check-number");
 
-checkNumberButton.addEventListener('click', function
-getValues(){
-    console.log(dateOfBirth.value, luckyNumber.value);
-})
+const outputBox = 
+document.querySelector("#output-box");
+
+
+function compareValue(sum,luckyNumber)
+{
+    if(sum%luckyNumber==0) {
+        outputBox.innerText="your birthday is lucky 🥳";
+    }else {
+        outputBox.innerText="your birthday is not lucky";
+
+    }
+}
+
+function checkBirthDteIsLucky(){
+    const dob =dateOfBirth.value;
+    const sum =calculateSum(dob);
+    compareValue(sum,luckyNumber.value)
+}
+
+function calculateSum(dob) {
+dob= dob.replaceAll("-","");
+let sum =0;
+for(let index=0;index<dob.length;
+    index++){
+        sum = sum + Number(dob.charAt
+            (index));
+    }
+    return sum;
+}
+
+checkNumberButton.addEventListener('click' ,checkBirthDteIsLucky)
